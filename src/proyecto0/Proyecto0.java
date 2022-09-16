3c82c358c12ed9de060601933af548d16b3f3f34
sanpatric <pintosandra244@gmail.com>
2022-09-15 23:16:23 -0500

    Se agrega proyecto

---
+++ src/proyecto0/Proyecto0.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto0;
+
+/**
+ *
+ * @author Sandra
+ */
+public class Proyecto0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 5;
+        int y = 4;
+        
+        int suma = x+y;
+        System.out.println("El resultado de la suma es: " + suma);
+    }
+    
+}
